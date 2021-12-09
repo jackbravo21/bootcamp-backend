07143319cf87618d975a2cdee1aec7fdf54175cd
jackbravo21 <kaldnar@hotmail.com>
2021-12-09 19:47:39 -0300

    Server Express

---
+++ src/server_teste.ts
@@ -0,0 +1,16 @@
+//const http = require("http");
+/*
+import http from "http";
+
+const server = http.createServer((req, res) =>{
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Funcionou!");
+});
+
+const port = 5000;
+
+server.listen(port, ()=>{
+    console.log("Server functionando na porta: ", port);
+});
+*/
